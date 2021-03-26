230d9371412f5a7b8c9d22fc806575d696009f99
Federico van Swaaij <federico.vanswaaij@gmail.com>
2021-03-26 11:04:19 +0100

    more arguments, better structure

--- src/utils/pdb_pipeline.py
+++ src/utils/pdb_pipeline.py
@@ -6,22 +6,24 @@ Created on Thu Mar 25 15:20:13 2021
 """
 
 import argparse
-from pdb_file import get_files, read_pdb_ids_file, fetch_pdbs
+from pdb_file import get_files, read_pdb_ids_file, fetch_pdb
 from sequence_cluster import match_clusters, get_clusters
+import progress_bar
 
-def pipeline(pdb_ids_list, out_dir, sqid=30, verbose=False):
+def pipeline(pdb_ids, out_dir, sqid=30, loaded=False, verbose=False):
     '''
     Performs a full PDB data pipeline.
 
     Parameters
     ----------
-    pdb_ids_list : list<list<string>>
-        A list containing a list of the PDB IDs to be downloaded. It is structured this way because for more
-        than 25000 IDs, the PDB website generates multiple .txt files of a maximum 25000 IDs each.
-    out_dir : string
+    pdb_ids_list : list<string>
+        A list of the PDB IDs to be downloaded.
+    out_dir : str
         The directory where to save the the PDB files.
     sqid : int, optional
         The sequence identity percentage for the sequence clustering. The default is 30.
+    loaded : bool, optional
+        Whether the clusters have already been loaded. The default is False.
     verbose : bool, optional
         Whether to print progress information. The default is False.
 
@@ -30,33 +32,38 @@ def pipeline(pdb_ids_list, out_dir, sqid=30, verbose=False):
     None.
     '''
     # Get clusters
-    clusters = get_clusters(verbose=verbose)
+    clusters = get_clusters(sqid=sqid, loaded=loaded, verbose=verbose)
     # Match the PDB IDs with the clusters
-    m_clusters = []
-    for pdb_ids in pdb_ids_list:
-        m_clusters.append(match_clusters(pdb_ids, clusters=clusters, sqid=sqid, verbose=verbose))
+    m_pdb_ch_ids = match_clusters(pdb_ids, clusters=clusters, sqid=sqid, verbose=verbose)
     # Fetch the PDB files
     count = 1
-    for m_cluster in m_clusters:
+    latest_bar = 1
+    if verbose:
+        print('Fetching PDBs...')
+        progress_bar.start()
+    for m_pdb_ch_id in m_pdb_ch_ids:
         if verbose:
-            print(f'Processing cluster {count}/{len(m_clusters)}')
+            latest_bar = progress_bar.progress(count, len(m_pdb_ch_ids), latest_bar)
             count += 1
-        fetch_pdbs(m_cluster, out_dir=out_dir, verbose=verbose)
+        fetch_pdb(m_pdb_ch_id, out_dir=out_dir)
+    if verbose:
+        progress_bar.end()
             
 
 if __name__ == '__main__':
     # Argument parser initialization
     arg_parser = argparse.ArgumentParser(description='Full PDB dataset pipeline')
-    arg_parser.add_argumennt('--ids_dir', type=str, default='../../pdb/ids/', help='The directory containing the .txt files holding the IDs for the PDB.')
-    arg_parser.add_argumennt('--out_dir', type=str, default='../../pdb/data/', help='The directory where to store the PDB files.')
-    arg_parser.add_argumennt('--verbose', type=bool, default=False, help='Whether to print progress information. The default is False.')
-    arg_parser.add_argumennt('--sqid', type=int, default=30, help='The sequence identity percentage for the sequence clustering. The default is 30.')
+    arg_parser.add_argument('--ids_dir', type=str, default='../../pdb/ids/', help='The directory containing the .txt files holding the IDs for the PDB.')
+    arg_parser.add_argument('--out_dir', type=str, default='../../pdb/data/', help='The directory where to store the PDB files.')
+    arg_parser.add_argument('--verbose', type=bool, default=False, help='Whether to print progress information. The default is False.')
+    arg_parser.add_argument('--sqid', type=int, default=30, help='The sequence identity percentage for the sequence clustering. The default is 30.')
+    arg_parser.add_argument('--loaded', type=bool, default=False, help='Whether the sequence clusters have already been loaded. The default is False.')
     # Parse arguments
     args = arg_parser.parse_args()
-    # Get list of PDBs
-    pdb_ids_list = []
-    files = get_files(args.pdb_ids_dir, ext='.txt')
+    # Get PDB IDs
+    pdb_ids = []
+    files = get_files(args.ids_dir, ext='.txt')
     for file in files:
-        pdb_ids_list.append(read_pdb_ids_file(file))
+        pdb_ids += read_pdb_ids_file(file)
     # Begin pipeline
-    pipeline(pdb_ids_list, args.out_dir, sqid=args.sqid, verbose=args.verbose)
+    pipeline(pdb_ids, args.out_dir, sqid=args.sqid, loaded=args.loaded, verbose=args.verbose)
