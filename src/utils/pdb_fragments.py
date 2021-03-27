409582d476731c18aaf89970480043236e4ca38d
Federico van Swaaij <federico.vanswaaij@gmail.com>
2021-03-27 11:02:49 +0100

    added script for fragments processing, not tested yet

---
+++ src/utils/pdb_fragments.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Mar 27 10:30:55 2021
+
+@author: FVS
+"""
+
+from pdb_file import merge_pdbs, get_files
+import progress_bar
+
+def merge_fragments(frag_dir, pdb_ch_ids, k=2, verbose=False):
+    '''
+    Merge individual fragments into non-redundant super-fragments. The super-fragments are composed by the
+    specified number of indivdual fragments.
+
+    Parameters
+    ----------
+    frag_dir : str
+        The directory holding the fragments.
+    pdb_ch_ids : list of (str, str)
+        A list of tuples, where each tuple is of the form (protein_id, chain_id). This parameter is necessary
+        because of where TERMANAL stores the fragments: <protein_id>_<chain_id>/fragments/.
+    k : int, optional
+        The number of individual fragments making up a super-fragment. The default is 2.
+    verbose : bool, optional
+        Whether to print progress information. The default is False.
+
+    Returns
+    -------
+    None.
+    '''
+    # Iterate over the PDB, chain IDs
+    count = 1
+    latest_bar = 1
+    if verbose:
+        print('Merging fragments...')
+        progress_bar.start()
+    for pdb_ch_id in pdb_ch_ids:
+        if verbose:
+            latest_bar = progress_bar.progress(count, len(pdb_ch_ids), latest_bar)
+            count += 1
+        p_id, c_id = pdb_ch_id
+        dir_name = frag_dir + p_id + '_' + c_id + '/fragments/'
+        pdb_frags = get_files(dir_name)
+        # Sort the list to get the correct order of the fragments
+        pdb_frags.sort()
