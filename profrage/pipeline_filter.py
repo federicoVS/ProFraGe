2b564af220fe0ed0e5dd3be75971d0416f2221fd
Federico van Swaaij <federico.vanswaaij@gmail.com>
2021-04-11 17:09:59 +0200

    big refactoring

--- profrage/pipeline_filter.py
+++ profrage/pipeline_filter.py
@@ -13,25 +13,25 @@ from fragments.filtering import is_complex, is_connected
 from utils.ProgressBar import ProgressBar
 from utils.io import get_files, from_mmtf
 
-def pipeline(fragments_dir, pdb_ch_ids, method, radius=5, grade=12, out_dir='./', verbose=False):
+def pipeline(method, base_dir, pdb_ch_ids, filtering_method, radius=5, grade=12, verbose=False):
     '''
     This pipeline takes a directory of fragments and filters them in order to retain ones which
     are composed of multiple segments.
 
     Parameters
     ----------
-    fragments_dir : str
-        The directory containing the fragments.
+    method : str
+        The method used to generate the fragments.
+    base_dir : str
+        The base directory for input and output.
     pdb_ch_ids : str
         The file holding the IDs of proteins and chains.
-    method : str
+    filtering_method : str
         The filtering method to apply to the fragments.
     radius : float, optional
         The minimal radius to consider two redidues belonging to different segments.The default is 5 A.
     grade : int, optional
         The minimal number of residues for a fragment to be complex. The default is 12.
-    out_dir : str, optional
-        The directory where to save the results. The default is './' (current directory).
     verbose : bool, optional
         Whether to print progress information. The default is False.
 
@@ -39,38 +39,63 @@ def pipeline(fragments_dir, pdb_ch_ids, method, radius=5, grade=12, out_dir='./'
     -------
     None.
     '''
+    # Define proper output directory
+    out_name = method + '-filtered/'
     count = 1
     progress_bar = ProgressBar()
     if verbose:
         print('Filtering the fragments...')
         progress_bar.start()
     n_fragments = 0
-    # Iterate over the protein, chain IDs
-    for pdb_ch_id in pdb_ch_ids:
-        if verbose:
-            progress_bar.step(count, len(pdb_ch_ids))
-            count += 1
-        p_id, c_id = pdb_ch_id
-        dir_name = fragments_dir + p_id + '_' + c_id + '/fragments/'
-        # Check if directory for the TERM indeed exists
-        if os.path.isdir(dir_name):
-            # Get all MMTF files holding the TERMs
-            mmtfs = get_files(dir_name, ext='.mmtf')
-            # Iterate through each MMTF file
-            for mmtf in mmtfs:
-                term = from_mmtf(mmtf)
-                if method == 'connected':
-                    if not is_connected(term, radius=radius):
-                        n_fragments += 1
-                        shutil.copy(mmtf, out_dir + os.path.basename(mmtf)[:-5] + '.mmtf')
-                elif method == 'complex':
-                    if is_complex(term, grade=grade):
-                        n_fragments += 1
-                        shutil.copy(mmtf, out_dir + os.path.basename(mmtf)[:-5] + '.mmtf')
-                else:
-                    if verbose:
-                        progress_bar.end()
-                    return
+    if method == 'terms' or method == 'seq' or method == 'ccg':
+        # Build proper output directory
+        if not os.path.exists(base_dir+out_name):
+            os.makedirs(base_dir+out_name)
+        # Iterate over the protein, chain IDs
+        for pdb_ch_id in pdb_ch_ids:
+            if verbose:
+                progress_bar.step(count, len(pdb_ch_ids))
+                count += 1
+            p_id, c_id = pdb_ch_id
+            dir_name = base_dir + method + '/' + p_id + '_' + c_id + '/fragments/'
+            # Check if directory for the TERM indeed exists
+            if os.path.isdir(dir_name):
+                # Get all MMTF files holding the TERMs
+                mmtfs = get_files(dir_name, ext='.mmtf')
+                # Iterate through each MMTF file
+                for mmtf in mmtfs:
+                    frag = from_mmtf(mmtf)
+                    if filtering_method == 'connected':
+                        if not is_connected(frag, radius=radius):
+                            n_fragments += 1
+                            shutil.copy(mmtf, base_dir + out_name + os.path.basename(mmtf)[:-5] + '.mmtf')
+                    elif filtering_method == 'complex':
+                        if is_complex(frag, grade=grade):
+                            n_fragments += 1
+                            shutil.copy(mmtf, base_dir + out_name + os.path.basename(mmtf)[:-5] + '.mmtf')
+                    else:
+                        print('Invalid filtering method.')
+                        return
+    elif method == 'fuzzle':
+        for root, _, files in os.walk(base_dir + method + '/'):
+            if verbose:
+                progress_bar.step(count, len(pdb_ch_ids))
+                count += 1
+            for name in files:
+                if name[:-4] == 'mmtf':
+                    mmtf = os.path.join(root, name)
+                    frag = from_mmtf(mmtf)
+                    if filtering_method == 'connected':
+                        if not is_connected(frag, radius=radius):
+                            n_fragments += 1
+                            shutil.copy(mmtf, base_dir + out_name + os.path.basename(mmtf)[:-5] + '.mmtf')
+                    elif filtering_method == 'complex':
+                        if is_complex(frag, grade=grade):
+                            n_fragments += 1
+                            shutil.copy(mmtf, base_dir + out_name + os.path.basename(mmtf)[:-5] + '.mmtf')
+                    else:
+                        print('Invalid filtering method.')
+                        return
     if verbose:
         progress_bar.end()
         print(f'Generated {n_fragments} fragments.')
@@ -79,10 +104,10 @@ def pipeline(fragments_dir, pdb_ch_ids, method, radius=5, grade=12, out_dir='./'
 if __name__ == '__main__':
     # Argument parser initialization
     arg_parser = argparse.ArgumentParser(description='Filtering the fragments.')
-    arg_parser.add_argument('fragments_dir', type=str, help='The directory where the fragments are held in MMTF format.')
-    arg_parser.add_argument('out_dir', type=str, help='The directory where the filtered fragments will be saved.')
+    arg_parser.add_argument('method', type=str, help='The method used to generate the fragments.')
+    arg_parser.add_argument('--base_dir', type=str, default='../pdb/fragments/', help='The base directory for input and output. The default is ../pdb/fragments/.')
     arg_parser.add_argument('--pdb_ch_ids', type=str, default='../pdb/ids/m_pdb_ch_ids', help='The file holding the IDs of the proteins and their chains. The default is ../pdb/ids/m_pdb_ch_ids')
-    arg_parser.add_argument('--method', type=str, default='complex', help='The filtering method to apply to the fragments. The options are [connected, complex]. The default is complex.')
+    arg_parser.add_argument('--filtering_method', type=str, default='complex', help='The filtering method to apply to the fragments. The options are [connected, complex]. The default is complex.')
     arg_parser.add_argument('--radius', type=float, default=5, help='The minimal radius in Angstroms to consider two residues belonging to different segments. The default is 5A.')
     arg_parser.add_argument('--grade', type=float, default=12, help='The minimal number of residues for a fragment to be complex. The default is 12.')
     arg_parser.add_argument('--verbose', type=bool, default=False, help='Whether to print progress information. The default is False.')
@@ -93,5 +118,5 @@ if __name__ == '__main__':
     with open(args.pdb_ch_ids, 'rb') as f:
         pdb_ch_ids = pickle.load(f)
     # Begin pipeline
-    pipeline(args.fragments_dir, pdb_ch_ids, args.method, args.radius, args.grade, out_dir=args.out_dir, verbose=args.verbose)
+    pipeline(args.method, args.base_dir, pdb_ch_ids, args.filtering_method, args.radius, args.grade, verbose=args.verbose)
     
--- profrage/pipeline_fragments.py
+++ profrage/pipeline_fragments.py
@@ -7,11 +7,11 @@ Created on Wed Apr  7 10:01:08 2021
 
 import os
 import argparse
-from fragments.generate import CCG, SeqG
+from fragments.generate import CCGen, SeqGen, FuzzleGen
 from utils.io import get_files, from_mmtf, from_pdb
 from utils.ProgressBar import ProgressBar
 
-def pipeline(method, data_dir, out_dir, radius, k, ext, verbose=False):
+def pipeline(method, data_dir, out_dir, radius, k, fuzzle_json, ext, verbose=False):
     '''
     This pipeline generates fragments from the specified proteins with the specified generation
     method.
@@ -30,6 +30,8 @@ def pipeline(method, data_dir, out_dir, radius, k, ext, verbose=False):
         The number of spectral partitions.
     ext : str
         The extension of the input files.
+    fuzzle_json : str
+        The JSON file describing the Fuzzle fragment network.
     verbose : bool, optional
         Whether to print progress information. The default is False.
 
@@ -37,51 +39,71 @@ def pipeline(method, data_dir, out_dir, radius, k, ext, verbose=False):
     -------
     None.
     '''
-    s_files = get_files(data_dir, ext=ext)
-    progress_bar = ProgressBar()
-    count = 1
-    if verbose:
-        print('Generating fragments...')
-        progress_bar.start()
-    for s_file in s_files:
+    if ext is not None:
+        s_files = get_files(data_dir, ext=ext)
+        progress_bar = ProgressBar()
+        count = 1
         if verbose:
-            progress_bar.step(count, len(s_files))
-            count += 1
-        structure = None
-        if ext == '.mmtf':
-            structure = from_mmtf(s_file)
-        elif ext == '.pdb':
-            structure = from_pdb(os.path.basename(s_file)[:-4], s_file, quiet=True)
-        else:
-            print('Invalid file extension.')
-            return
+            print('Generating fragments...')
+            progress_bar.start()
+        for s_file in s_files:
+            if verbose:
+                progress_bar.step(count, len(s_files))
+                count += 1
+            structure = None
+            if ext == '.mmtf':
+                structure = from_mmtf(s_file)
+            elif ext == '.pdb':
+                structure = from_pdb(os.path.basename(s_file)[:-4], s_file, quiet=True)
+            # Get generation method
+            generator = None
+            if method == 'ccg':
+                generator = CCGen(structure, radius)
+            elif method == 'seq':
+                generator = SeqGen(structure, k)
+            else:
+                print('Invaid generation method')
+                return
+            # Generate the fragments
+            generator.generate()
+            # Build proper output directory
+            dir_name = method + '/'
+            if not os.path.exists(out_dir+dir_name):
+                os.makedirs(out_dir+dir_name)
+            # Save the fragments in the specified directory
+            generator.save(out_dir+dir_name)
+        if verbose:
+            progress_bar.end()
+    else:
+        # Get generation method
         generator = None
-        if method == 'ccg':
-            generator = CCG(structure, radius)
-        elif method == 'seq':
-            generator = SeqG(structure, k)
+        if method == 'fuzzle':
+            generator = FuzzleGen(fuzzle_json, verbose=verbose)
         else:
             print('Invaid generation method')
             return
         # Generate the fragments
         generator.generate()
+        # Build proper output directory
+        dir_name = method + '/'
+        if not os.path.exists(out_dir+dir_name):
+            os.makedirs(out_dir+dir_name)
         # Save the fragments in the specified directory
-        generator.save(out_dir)
-    if verbose:
-        progress_bar.end()
+        generator.save(out_dir+dir_name)
 
 
 if __name__ == '__main__':
     # Argument parser initialization
     arg_parser = argparse.ArgumentParser(description='Generation of fragments.')
-    arg_parser.add_argument('method', type=str, help='The method to be used to the generate the fragments. The options are [ccg, seq].')
-    arg_parser.add_argument('out_dir', type=str, help='The directory where the fragments will be saved.')
-    arg_parser.add_argument('--data_dir', type=str, default='../pdb/data/', help='The directory containing the proteins in MMTF format. The default is ../pdb/data/')
+    arg_parser.add_argument('method', type=str, help='The method to be used to the generate the fragments. The options are [ccg, seq, fuzzle, terms].')
+    arg_parser.add_argument('--out_dir', type=str, default='../pdb/fragments/', help='The base directory where the fragments will be saved. the default is ../pdb/fragments/.')
+    arg_parser.add_argument('--data_dir', type=str, default='../pdb/data/', help='The directory containing the proteins. The default is ../pdb/data/')
     arg_parser.add_argument('--radius', type=float, default=3, help='The cutoff radius in Angstroms. This option is only effective when method `seq` is chosen. The default is 3.')
-    arg_parser.add_argument('--k', type=int, default=10, help='The number of spectral partitions. This option is only effective when method `knn` is chosen. The default is 10.')
-    arg_parser.add_argument('--ext', type=str, default='.mmtf', help='The format of the input files. The deafult is .mmtf.')
+    arg_parser.add_argument('--k', type=int, default=10, help='The number of connected components/contiguous sequences. The default is 10.')
+    arg_parser.add_argument('--fuzzle_json', type=str, default='../pfg-fuzzle/psiblast2.06.1.25-3.json', help='The JSON file describing the Fuzzle fragment network. This option is only effective when method `fuzzle` is chosen. The default is ../pfg-fuzzle/psiblast2.06.1.25-3.json.')
+    arg_parser.add_argument('--ext', type=str, default=None, help='The format of the input files. The options are [.mmtf, .pdb]. Note that methods `ccg` and `seq` need one of them. The deafult is None.')
     arg_parser.add_argument('--verbose', type=bool, default=False, help='Whether to print progress information. The default is False.')
     # Parse arguments
     args = arg_parser.parse_args()
     # Begin pipeline
-    pipeline(args.method, args.data_dir, args.out_dir, args.radius, args.k, args.ext, verbose=args.verbose)
+    pipeline(args.method, args.data_dir, args.out_dir, args.radius, args.k, args.fuzzle_json, args.ext, verbose=args.verbose)
