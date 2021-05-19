276d23aaeaa1ed6007cb58b55f76308d34c61a12
Federico van Swaaij <federico.vanswaaij@gmail.com>
2021-05-19 21:22:28 +0200

    changed location

--- profrage/tuning/__init__.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Fri May 14 17:23:02 2021
-
-@author: Federico van Swaaij
-"""
-
--- profrage/tuning/tune.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Mon May 17 11:22:23 2021
-
-@author: Federico van Swaaij
-"""
-
