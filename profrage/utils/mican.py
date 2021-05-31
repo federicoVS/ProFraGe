c931dc0daf8aefcbe357243dc933363948fae16a
Federico van Swaaij <federico.vanswaaij@gmail.com>
2021-05-31 17:37:39 +0200

    fixed bug

--- profrage/utils/mican.py
+++ profrage/utils/mican.py
@@ -1,9 +1,8 @@
-#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
 Created on Fri May 21 16:22:11 2021
 
-@author: FVS
+@author: Federico van Swaaij
 """
 
 import subprocess
@@ -30,10 +29,12 @@ def run_mican(mican_dir, pdb_1, pdb_2):
     ps = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
     output = ps.communicate()[0]
     lines = output.split(b'\n')
-    for line in lines:
-        line = line.decode('utf-8')
-        if line.startswith('TM-score'):
-            scores = line.split('=')[1]
-            tm_score = float(scores.split()[0][:-1]) # take out the comma at the end
-            return tm_score
+    if len(lines) < 9:
+        return 0
+    score_lines = lines[8].decode('utf-8').split()
+    if len(score_lines) == 0:
+        return 0
+    if score_lines[0] == '1': # only insterested in rank 1 (i.e. the best)
+        tm_score = float(score_lines[2])
+        return tm_score
     return 0
